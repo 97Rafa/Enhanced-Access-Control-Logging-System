7c4715c16cd3a09ba9c9ad9121b23a1d80b6a283
Rafail <raf@pop-os.localdomain>
2022-11-27 11:23:52 +0200

    file_log was created with uid,filename,date,timestamp,access_type,hash(?)

--- logger.c
+++ logger.c
@@ -8,24 +8,40 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <openssl/md5.h>
+#include <stdbool.h>
 
-FILE *
-fopen(const char *path, const char *mode) 
+bool wrote=false;
+
+// original fopen
+FILE*
+fopen_FOR_REAL(const char *path, const char *mode) 
 {
 
 	FILE *original_fopen_ret;
 	FILE *(*original_fopen)(const char*, const char*);
 
-	/* call the original fopen function */
 	original_fopen = dlsym(RTLD_NEXT, "fopen");
 	original_fopen_ret = (*original_fopen)(path, mode);
 
+	return original_fopen_ret;
+}
+
+// fopen on steroids(i mean it's logging...)
+FILE*
+fopen(const char *path, const char *mode) 
+{
+
+	FILE *original_fopen_ret;
+	/* call the original fopen function */
+	original_fopen_ret = fopen_FOR_REAL(path, mode);
+
 	// Logging the action
 
-	char *uid;
+	int uid;
 	char date[20];
     char timestamp[20];
 	uid = getuid();
+	int accType;
 	
     time_t t;
     t = time(NULL);
@@ -33,16 +49,53 @@ fopen(const char *path, const char *mode)
     sprintf(date,"%d-%d-%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
     sprintf(timestamp,"%d:%d", tm.tm_hour, tm.tm_min);
 
-	
-	// unsigned char *MD5(const unsigned char *d, unsigned long n, unsigned char *md);
+	if(strcmp(mode, "r") == 0){
+		accType = 1;
+	}else if(strcmp(mode, "w") == 0){
+		if(original_fopen_ret != NULL){ //the file exists
+			accType = 3; 
+		}else{ 							//the file does not exist
+			accType = 0;
+		}
+	}else if(strcmp(mode, "a") == 0){
+		accType = 2;
+	}else if(strcmp(mode, "r+") == 0){
+		if(wrote ==  true){ 			//fwrite() was used
+			accType = 2; 
+		}else{		
+			accType = 1;
+		}
+	}else if(strcmp(mode, "w+") == 0){
+		if(original_fopen_ret != NULL){ //the file exists
+			if(wrote==true){ 			//fwrite() was used
+				accType = 3; 
+			}else{
+				accType = 1;
+			}			
+		}else{ 							//the file does not exist
+			accType = 0;
+		}
+	}else if(strcmp(mode, "a+") == 0){
+		if(original_fopen_ret != NULL){ //the file exists
+			if(wrote==true){ 			//fwrite() was used
+				accType = 2; 
+			}else{
+				accType = 1;
+			}			
+		}else{ 							//the file does not exist
+			accType = 0;
+		}
+	}
 
-	// int MD5_Init(MD5_CTX *c);
-	// int MD5_Update(MD5_CTX *c, const void *data, unsigned long len);
-	// int MD5_Final(unsigned char *md, MD5_CTX *c);
+
+	
+	
 
 	//	Writing to the log file
 	FILE *fptr;
-	fptr = fopen("file_logging.log","a");
+	
+	fptr = fopen_FOR_REAL("file_logging.log","a");
+	
 
 	if(fptr == NULL)
 	{
@@ -50,7 +103,7 @@ fopen(const char *path, const char *mode)
 		exit(1);             
 	}
 
-	fprintf(fptr,"%s | %s | %s | %s",uid, path, date, timestamp);
+	fprintf(fptr,"%d | %s | %s | %s | %d | ",uid, path, date, timestamp, accType);
 	fclose(fptr);
 
 	return original_fopen_ret;
@@ -69,20 +122,41 @@ fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 	original_fwrite_ret = (*original_fwrite)(ptr, size, nmemb, stream);
 
 	// Logging the action
+	wrote=true;
 
-	char *uid;
-	char date[20];
-    char timestamp[20];
-	uid = getuid();
+    // MD5_Init (&mdContext);
+    // while ((bytes = fread (data, 1, 1024, stream)) != 0){
+    //     MD5_Update (&mdContext, data, bytes);
+	// 	}
+    // MD5_Final (c,&mdContext);
+	MD5_CTX mdContext;
+    int bytes;
+    unsigned char data[1024];
+	unsigned char c[MD5_DIGEST_LENGTH];
+
+    MD5_Init(&mdContext);
+    while ((bytes = fread (data, 1, 1024, stream)) != 0){
+        MD5_Update(&mdContext, data, bytes);
+		}
+    MD5(c,MD5_DIGEST_LENGTH,&mdContext);
+
 	
-    time_t t;
-    t = time(NULL);
-    struct tm tm = *localtime(&t);
-    sprintf(date,"%d-%d-%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
-    sprintf(timestamp,"%d:%d", tm.tm_hour, tm.tm_min);
 
+	
+	FILE *fptr;
+	fptr = fopen_FOR_REAL("file_logging.log","a");
+    
+	if(fptr == NULL)
+	{
+		printf("Error!");   
+		exit(1);             
+	}	
+	for(int i = 0; i < MD5_DIGEST_LENGTH; i++)  fprintf(fptr,"%02x",c[i]);
+	fprintf(fptr,"\n");
+	
+	fclose(fptr);
 
-
+	
 	return original_fwrite_ret;
 }
 
--- test.c
+++ test.c
@@ -11,29 +11,52 @@
 
 int main(){
     // Logging the action
-    char *path = "randomfile.txt";
+    char *path = "test.txt";
 	char uid[100];
 	char date[20];
     char timestamp[20];
+
+
 	// uid = itoa(getuid());
     sprintf(uid,"%d", getuid());
 	
     time_t t;
     t = time(NULL);
     struct tm tm = *localtime(&t);
-    sprintf(date,"%d-%d-%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
-    sprintf(timestamp,"%d:%d", tm.tm_hour, tm.tm_min);
+    sprintf(date,"%02d-%02d-%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
+    sprintf(timestamp,"%02d:%02d", tm.tm_hour, tm.tm_min);
 
+	
+	unsigned char c[MD5_DIGEST_LENGTH];
+    
+    FILE *inFile = fopen ("malakies.txt", "a");
+    MD5_CTX mdContext;
+    int bytes;
+    unsigned char data[1024];
+
+    MD5_Init(&mdContext);
+    while ((bytes = fread (data, 1, 1024, inFile)) != 0){
+        MD5_Update(&mdContext, data, bytes);
+		}
+    MD5_Final(c,&mdContext);
+	fclose (inFile);
+
+	
+
+	
 	FILE *fptr;
 	fptr = fopen("file_logging.log","a");
-
+    
 	if(fptr == NULL)
 	{
 		printf("Error!");   
 		exit(1);             
 	}
-
-	fprintf(fptr,"%s | %s | %s | %s",uid, path, date, timestamp);
+	fprintf(fptr,"%s | %s | %s | %s | ",uid, path, date, timestamp);
+	
+	for(int i = 0; i < MD5_DIGEST_LENGTH; i++)  fprintf(fptr,"%02x",c[i]);
+	fprintf(fptr,"\n");
+	
 	fclose(fptr);
 
 
